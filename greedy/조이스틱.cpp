88ddb094f4fa11d18961bb8204f269024c03e01f
seungmin001 <51369785+seungmin001@users.noreply.github.com>
2021-02-16 21:42:37 +0900

    0과 index[0]사이 이동 앞 뒤 고려구문 추가

--- greedy/조이스틱.cpp
+++ greedy/조이스틱.cpp
@@ -3,8 +3,15 @@
 #include<vector>
 using namespace std;
 
-//'A'에서 얼마나 이동해야하는가 반환
-int move_num(char ch) { 
+int move_num(char ch) {
+    //if(index[0]==0){
+        // if(name[0]-'A' <= 13){ //위로
+        //     answer+=name[0]-'A';
+        // }
+        // else{ //아래로
+        //     answer+='Z'-name[0]+1; //'A'에서 'Z'로 가는 횟수 +1
+        // }
+    //}
     if (ch - 'A' <= 13) { //위로
         cout << "상하 " << ch - 'A' << endl;
         return ch - 'A';
@@ -22,15 +29,17 @@ int solution(string name) {
         if (name[i] != 'A')
             index.push_back(i);
     }
+
     
     if (index[0] != 0) {
-        //처음 위치는 0이므로, 0과 index[0]사이 이동
-        answer += index[0];
+        //0과 index[0]사이 이동
+        int temp = index[0] < name.size() - index[0] ? index[0] : name.size() - index[0]; //앞 뒤 더 작은거
+        answer += temp;
     }
     
     bool isfront = true; //현재 커서가 맨 앞인가 / false : 맨 뒤
     while (!index.empty()) {
-        //상하이동(알파벳)
+        //상하이동
         if (isfront) {
             answer += move_num(name[index[0]]);
         }
@@ -40,12 +49,12 @@ int solution(string name) {
 
         //좌우 이동
         if (index.size() > 1) {
-            int front = 0; //index로 가기 위해서 오른쪽으로 이동
-            int back = 0; //왼쪽으로 이동
-            if (isfront) { //현재 첫번째 요소위치면
-                front = index[1] - index[0]; //2번째요소까지 거리
-                back = name.size() - (index.back() - index[0]); //마지막요소까지 거리
-                index.erase(index.begin()); //값 구했으므로 벡터에서 지움
+            int front = 0; //index로 가기 위해서 앞으로 이동
+            int back = 0; //뒤로 이동
+            if (isfront) {
+                front = index[1] - index[0];
+                back = name.size() - (index.back() - index[0]);
+                index.erase(index.begin());
             }
             else {
                 front = name.size() - (index.back() - index[0]); //오른쪽진행
@@ -53,22 +62,28 @@ int solution(string name) {
                 index.erase(index.end() - 1);
             }
 
-            if (front <= back) { //앞으로가는게 더 짧음. 다음 위치는 맨 앞
+            if (front <= back) { //앞으로가는게 더 짧음
                 isfront = true;
-                //cout << "앞 " << front << endl;
+                cout << "앞 " << front << endl;
                 answer += front;
             }
-            else { //다음 기준 위치 맨 뒤
+            else {
                 isfront = false;
-                //cout << "뒤 " << back << endl;
+                cout << "뒤 " << back << endl;
                 answer += back;
             }
         }
-        else { //하나 남았으므로 비교할 필요 없어서 빼고 while문 빠져나가기
+        else {
             index.pop_back();
         }
 
     }
 
     return answer;
+}
+
+int main() {
+
+    cout << solution("AAB") << endl;
+    return 0;
 }
