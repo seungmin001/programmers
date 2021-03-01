41f022f9a761b025f665aabcd7392f8aec0e1af7
seungmin001 <51369785+seungmin001@users.noreply.github.com>
2021-03-01 22:10:33 +0900

    Create 프린터2.py

---
+++ stack&queue/프린터2.py
@@ -0,0 +1,27 @@
+from collections import deque
+def solution(priorities, location):
+    answer = 0
+    
+    deq=deque(priorities) # deque
+    count=0 # 빠져나간 개수
+    l=location # location값
+    
+    while deq:
+        m = max(deq) # 현재 우선순위 max
+        cur=deq.popleft() # 현재 맨 앞
+        if cur < m : # 우선순위 낮음
+            deq.append(cur)
+            # location 한 칸 앞으로
+            if l==0:
+                l=len(deq)-1
+            else:
+                l-=1
+        else: # 우선순위 가장 높음
+            count+=1
+            if l==0:
+                return count
+            else:
+                l-=1
+            
+
+    return answer
